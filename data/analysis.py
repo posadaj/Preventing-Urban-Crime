11a2ad8a77959b7bb6dec2acd086d39446002198
Jonathan Posada <jnp53@cornell.edu>
2016-12-01 15:51:17 -0500

    Linear regression and regularization

--- data/analysis.py
+++ data/analysis.py
@@ -1,18 +1,54 @@
 """ Perform analysis on cleaned data. """
 
-import csv, numpy
+import csv
+import numpy as np
+from sklearn import linear_model, preprocessing
+
 
 # Input data
 reader = csv.reader(open("zipcode_2015_train_matrix.csv","rb"), delimiter=",") 
-X_list = list(reader)
-X=numpy.array(X_list).astype('float')
 
+# Extract X, y from csv
+raw_data = np.array(list(reader)).astype('float')
+_, cols = raw_data.shape
+y = raw_data[:, cols-1]  
+X = raw_data[:, 0:cols-1]  # Remove output y from X
+X = X[:, :4]        # Remove time distribution of crimes data
 
-"""
+# Standardize data
+X = preprocessing.scale(X)
+
 # Linear reagression
-from sklearn import linear_model
 reg = linear_model.LinearRegression()
 reg.fit(X, y)
-w = reg.coef_
-print( "Model w is {}".format(w) )
-"""
+w_reg = reg.coef_
+
+# Ridge regression
+ridge_reg = linear_model.Ridge( alpha = 0.5 )
+ridge_reg.fit(X, y)
+w_ridge_reg = ridge_reg.coef_
+
+# Ridge regression with CV
+ridge_reg_cv = linear_model.RidgeCV( alphas = [0.2, 0.5, 1, 10, 20] )
+ridge_reg_cv.fit(X, y)
+w_ridge_reg_cv = ridge_reg_cv.coef_
+print(ridge_reg_cv.alpha_)
+
+# Display results
+
+# Init
+np.set_printoptions(precision=3) # Reduce precision for displaying w
+threshold = 10**-2               # Remove 0 valued parameters
+
+# Threshold data
+w_reg = w_reg*(w_reg>threshold)
+w_ridge_reg = w_ridge_reg*(w_ridge_reg>threshold)
+w_ridge_reg_cv = w_ridge_reg_cv*(w_ridge_reg_cv>threshold)
+
+
+# Display model
+print( "Model w_reg is          {}".format(w_reg) )
+print( "Model w_ridge_reg is    {}".format(w_ridge_reg) )
+print( "Model w_ridge_reg_cv is {}".format(w_ridge_reg_cv) )
+
+
