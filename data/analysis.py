68f81836acd89879353b4016c063a4b180a44e48
Jonathan Posada <jnp53@cornell.edu>
2016-12-03 01:42:23 -0500

    More analysus

--- data/analysis.py
+++ data/analysis.py
@@ -2,21 +2,24 @@
 
 import csv
 import numpy as np
-from sklearn import linear_model, preprocessing
+from sklearn import linear_model, preprocessing, metrics
 
 
 # Input data
 reader = csv.reader(open("zipcode_2015_train_matrix.csv","rb"), delimiter=",") 
 
-# Extract X, y from csv
+# Extract X, y from data
 raw_data = np.array(list(reader)).astype('float')
 _, cols = raw_data.shape
-y = raw_data[:, cols-1]  
-X = raw_data[:, 0:cols-1]  # Remove output y from X
-X = X[:, :4]        # Remove time distribution of crimes data
 
 # Standardize data
-X = preprocessing.scale(X)
+X = preprocessing.scale(raw_data)
+y = X[:, cols-1]  
+X = X[:, 0:cols-1]  # Remove output y from X
+
+# Only consider certain features
+#X = X[:, :4]        # Remove time distribution of crimes data
+#X = X[:, 4:]
 
 # Linear reagression
 reg = linear_model.LinearRegression()
@@ -32,23 +35,39 @@ w_ridge_reg = ridge_reg.coef_
 ridge_reg_cv = linear_model.RidgeCV( alphas = [0.2, 0.5, 1, 10, 20] )
 ridge_reg_cv.fit(X, y)
 w_ridge_reg_cv = ridge_reg_cv.coef_
-print(ridge_reg_cv.alpha_)
+
+# Lasso
+reg_lasso = linear_model.Lasso( alpha = 1 )
+reg_lasso.fit(X, y)
+w_lasso = reg_lasso.coef_
 
 # Display results
 
 # Init
 np.set_printoptions(precision=3) # Reduce precision for displaying w
-threshold = 10**-2               # Remove 0 valued parameters
+"""
+threshold = 10**-3               # Remove 0 valued parameters
 
 # Threshold data
+
 w_reg = w_reg*(w_reg>threshold)
 w_ridge_reg = w_ridge_reg*(w_ridge_reg>threshold)
 w_ridge_reg_cv = w_ridge_reg_cv*(w_ridge_reg_cv>threshold)
+"""
 
 
 # Display model
 print( "Model w_reg is          {}".format(w_reg) )
 print( "Model w_ridge_reg is    {}".format(w_ridge_reg) )
 print( "Model w_ridge_reg_cv is {}".format(w_ridge_reg_cv) )
+print( "Model w_lasso is        {}".format(w_lasso) )
 
+
+
+# Calculate E_in
+score_cv = metrics.mean_absolute_error(y, np.dot(X, w_ridge_reg_cv) ) 
+
+# Dispaly E_in
+print( "Model w_ridge_reg_cv E_in: {}".format(score_cv) )
+print( np.dot(X, w_ridge_reg_cv) )
 
