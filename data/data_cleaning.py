8278de475051fc304f842f601cfbca8a70c52257
Jonathan Posada <jnp53@cornell.edu>
2016-12-04 13:02:24 -0500

    More work

--- data/data_cleaning.py
+++ data/data_cleaning.py
@@ -7,16 +7,6 @@ import googlemaps
 KEY = "AIzaSyAQ6v5twZhG5xUpjRlXcSMH9RLTzE55qvI"
 gmaps = googlemaps.Client(key=KEY)
 
-# Philly zip codes from
-# http://www.city-data.com/zipmaps/Philadelphia-Pennsylvania.html
-"""
-PHILLY_ZIP_CODES = [
-  19102, 19103, 19104, 19106, 19107, 19109, 19111, 19112, 19114, 19115, 
-  19116, 19118, 19119, 19120, 19121, 19122, 19123, 19124, 19125, 19126, 
-  19127, 19128, 19129, 19130, 19131, 19132, 19133, 19134, 19135, 19136, 
-  19137, 19138, 19139, 19140, 19141, 19142, 19143, 19144, 19145, 19146, 
-  19147, 19148, 19149, 19150, 19151, 19152, 19153, 19154]
-"""
 
 def main( in_fname, lon_idx, lat_idx, lon_lat_fname, out_fname ):
 
@@ -115,9 +105,7 @@ if __name__ == '__main__':
     python data_cleaning <filename> <lon_idx> <lat_idx>
     """
 
-    # python data_cleaning.py PPR_StreetTrees.csv 0 1
-    # python data_cleaning.py theft.csv 11 12
-    # python data_cleaning.py 2014crimetrainingdata.csv 12 13
+    # python data_cleaning.py 2015crimetestdata.csv 12 13
 
 
     # Check that script called correctly
